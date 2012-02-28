22490e7b1cdc68ab4fe4f5b5685c0ad69167d2a3
nonakaryuichi <nonaka.ryuichi@nonakaryuichi.me>
2012-02-28 09:38:11 +0900

    RSS取得時のポーリング設定済み

--- js/class.js
+++ js/class.js
@@ -1,111 +1,301 @@
 //get feed api
-(function($){
-	var feeds = [];
+google.load("feeds", "1");
+var App = {};
 
-	google.load("feeds", "1");
+//view
+App.minReader = function(){
+	this.options = {
+		state: 0
+	}
+	this.names = {
+		storage: {
+			feeds   : "feeds",
+			articles: "articles"
+		}
+	}
 
-	function initialize(feedURL) {
+	//storage
+	this.storage = new this.model(this.names);
+}
+
+//initialize
+App.minReader.prototype.init = function(){
+	var method = this;
+	var feeds = $.parseJSON(localStorage[this.names.storage.feeds]);
+	var feedCount = 0;
+
+	//var dom
+	var $feedList = $("#feedList");
+	
+	//set feeds
+	if(feeds){
+		this.storage.feeds(feeds);
+	} else {
+		this.storage.feeds(Array());
+	}
+
+
+	this.storage.articles(Array());
+	this.loader();
+
+	//add event add feed
+	$("#feedURLSet #feedURLSubmit").click(function(){
+		var url = $("#feedURLInput").val();
+		if(url.match(/(http|ftp):\/\/[!#-9A-~]+\.+[a-z0-9]+/i)){
+			method.addFeeds(0, url);
+		} else {
+			alert("URL Error");
+		}
+	});
+
+	this.loaderChecker();
+}
+
+App.minReader.prototype.loaderChecker = function(){
+	var method = this;
+	var articles = method.storage.articles();
+	var feeds    = method.storage.feeds();
+
+	if(feeds.length === articles.length){
+		method.viewFeedList();
+		return;
+	}
+
+	setTimeout(function(){
+		console.log("load");
+		method.loaderChecker();
+	},100);
+}
+
+//load Feeds
+App.minReader.prototype.loader = function(){
+	var method = this;
+	var feeds = this.storage.feeds();
+	
+	for(var i = 0; i < feeds.length; i++){
 		//new feed
-		var feed = new google.feeds.Feed(feedURL);
+		var feed = new google.feeds.Feed(feeds[i]);
 		var length = 10;
 
 		//feed length
 		feed.setNumEntries(length);
-		
 		//feed load
 		feed.load(function(result) {
 			if (!result.error) {
-				//var & reset
-				var $continer = $("#feedContiner");
-				$continer.find("*").remove();
-
-				//entry title
-				$continer.append(
-					$("<h2>")
-						.addClass("feedTitle")
-						.attr("title", result.feed.description)
-						.html(result.feed.title)
-						.append(
-							$("<span>")
-								.addClass("count")
-								.html("（" + result.feed.entries.length + "）")
-						)
-				);
-
-				//feed roop
-				for (var i = 0; i < result.feed.entries.length; i++) {
-					
-					//var
-					var entry    = result.feed.entries[i];
-					//console.log(result.feed);
-
-					//DOM chenge
-					$continer
-					.append(
-						$("<article>")
-							.attr("id","feedItem" + i)
-							.addClass("article")
-							/*
-							.append(
-								$("<ul>")
-									.addClass("command")
-									.append(
-										$("<li>")
-											.addClass("item")
-											.append(
-												$("<a>")
-													.attr("href","javascript:void(0)")
-													.data("readmore",entry.link)
-													.data("feedid",i)
-													.html("続きを読む")
-													.click(function(){
-														var $this = $(this);
-														var $body = $("#feedItem" + $this.data("feedid"));
-														console.log($body);
-														$body.load($this.data("readmore"));
-													})
-											)
-									)
-							)
-							*/
-							.append(
-								$("<h3>")
-								.html(entry.title)
-								.addClass("title")        
-							)
-							.append(
-								$("<time>")
-									.html(entry.publishedDate)
-									.addClass("time")
-									.attr("datetime",entry.publishedDate)
-							)
-							.append(
-								$("<div>")
-									.html(entry.content)
-									.addClass("content")
-							).append(
-								$("<p>")
-									.append(
-										$("<a>")
-										.attr("href",entry.link)
-										.html(entry.title)
-									)
-									.addClass("link")
-							)
-					);
-				}
+				var data = {};
+				data.title       = result.feed.title;
+				data.description = result.feed.description;
+				data.feedUrl     = result.feed.feedUrl;
+				data.url         = result.feed.link;
+				data.items       = result.feed.entries;
+				console.log(data);
+				method.addArticles(0, data);
+			} else {
+				alert("Error");
 			}
 		});
 	}
 
-	//load initialize
-	google.setOnLoadCallback(function(){
-		$("#feedList li a").click(function(){
-			$this = $(this);
+	
 
-			//drow
-			initialize($this.data('feedlink'));
-		});
-		
+}
+
+//feed Counter
+App.minReader.prototype.feedCounter = function(){
+	var feeds = this.storage.feeds();
+	return feeds.length;
+}
+
+//view
+App.minReader.prototype.viewFeedList = function(){
+	var articles = this.storage.articles();
+	var feeds = this.storage.feeds();
+	console.log(feeds);
+	console.log(this.storage.articles());
+	for(var i in articles){
+		var feed = articles[i];
+		console.log(feed);
+		$("#feedList").append(
+			$("<li>")
+				.addClass("item")
+				.append(
+					$("<a>")
+						.attr("href", "javascript:void(0)")
+						.data("feedlink", feed['feedUrl'])
+						.html(feed['title'])
+				)
+					
+		);
+	}
+}
+
+//model
+App.minReader.prototype.model = function(names){
+	var feeds;
+	var articles;
+
+	return {
+		feeds: function(d){
+			if(d){
+				feeds = d;
+				localStorage[names.storage.feeds] = $.stringify(feeds);
+			}
+			return feeds;
+		},
+		articles: function(d){
+			if(d){
+				articles = d;
+			}
+			return articles;
+		}
+	}
+}
+
+//add Feeds
+App.minReader.prototype.addFeeds = function(key, data){
+	var feeds =  this.storage.feeds();
+	if(key){
+		feeds[key] = data;
+	} else {
+		feeds.push(data);
+	}
+	this.storage.feeds(feeds);
+}
+
+//remove Feeds
+App.minReader.prototype.removeFeeds = function(key){
+	var feeds =  this.storage.feeds();
+	delete feeds[key];
+
+	this.storage.feeds(feeds);
+}
+
+
+//add Articles
+App.minReader.prototype.addArticles = function(key, data){
+	var articles =  this.storage.articles();
+	if(key){
+		articles[key] = data;
+	} else {
+		articles.push(data);
+	}
+
+	this.storage.articles(articles);
+	console.log(articles.length);
+}
+
+//remove Articles
+App.minReader.prototype.removeArticles = function(key){
+	var articles =  this.storage.articles();
+	delete articles[key];
+
+	this.storage.articles(articles);
+}
+
+
+
+function initialize(feedURL) {
+	//new feed
+	var feed = new google.feeds.Feed(feedURL);
+	var length = 10;
+
+	//feed length
+	feed.setNumEntries(length);
+	
+	//feed load
+	feed.load(function(result) {
+		if (!result.error) {
+			//var & reset
+			var $continer = $("#feedContiner");
+			$continer.find("*").remove();
+
+			//entry title
+			$continer.append(
+				$("<h2>")
+					.addClass("feedTitle")
+					.attr("title", result.feed.description)
+					.html(result.feed.title)
+					.append(
+						$("<span>")
+							.addClass("count")
+							.html("（" + result.feed.entries.length + "）")
+					)
+			);
+
+			//feed roop
+			for (var i = 0; i < result.feed.entries.length; i++) {
+				
+				//var
+				var entry    = result.feed.entries[i];
+				//console.log(result.feed);
+
+				//DOM chenge
+				$continer
+				.append(
+					$("<article>")
+						.attr("id","feedItem" + i)
+						.addClass("article")
+						.append(
+							$("<ul>")
+								.addClass("command")
+								.append(
+									$("<li>")
+										.addClass("item")
+										.append(
+											$("<a>")
+												.attr("href","javascript:void(0)")
+												.data("readmore",entry.link)
+												.data("feedid",i)
+												.html("続きを読む")
+												.click(function(){
+													var $this = $(this);
+													var $body = $("#feedItem" + $this.data("feedid"));
+													console.log($body);
+													$body.load($this.data("readmore"));
+												})
+										)
+								)
+						)
+						.append(
+							$("<h3>")
+							.html(entry.title)
+							.addClass("title")        
+						)
+						.append(
+							$("<time>")
+								.html(entry.publishedDate)
+								.addClass("time")
+								.attr("datetime",entry.publishedDate)
+						)
+						.append(
+							$("<div>")
+								.html(entry.content)
+								.addClass("content")
+						).append(
+							$("<p>")
+								.append(
+									$("<a>")
+									.attr("href",entry.link)
+									.html(entry.title)
+								)
+								.addClass("link")
+						)
+				);
+			}
+		}
 	});
-})(jQuery);
+}
+
+//load initialize
+google.setOnLoadCallback(function(){
+	var minReader = new App.minReader();
+	minReader.init();
+
+	$("#feedList li a").live('click',function(){
+		$this = $(this);
+
+		//drow
+		initialize($this.data('feedlink'));
+	});
+	
+});
