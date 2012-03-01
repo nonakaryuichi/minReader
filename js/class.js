d4829d2868b9e4eb9aea7642c517093d223ce5f5
nonakaryuichi <nonaka.ryuichi@nonakaryuichi.me>
2012-03-02 02:00:11 +0900

    add and remove feed

--- js/class.js
+++ js/class.js
@@ -13,127 +13,46 @@ App.minReader = function(){
 			articles: "articles"
 		}
 	}
+	this.$ = {
+		feedList : $("#feedList")
+	}
 
 	//storage
 	this.storage = new this.model(this.names);
 }
 
-//initialize
-App.minReader.prototype.init = function(){
+//controler
+App.minReader.prototype.controler = function(){
 	var method = this;
-	var feeds = $.parseJSON(localStorage[this.names.storage.feeds]);
-	var feedCount = 0;
 
-	//var dom
-	var $feedList = $("#feedList");
-	
-	//set feeds
-	if(feeds){
-		this.storage.feeds(feeds);
-	} else {
-		this.storage.feeds(Array());
-	}
+	this.$.feedList.find("li .selector").live('click', function(){
+		$this = $(this);
 
+		//drow
+		initialize($this.data('feedlink'));
+	});
 
-	this.storage.articles(Array());
-	this.loader();
+	this.$.feedList.find("li .delete").live('click', function(){
+		$this = $(this);
+
+		//drow
+		method.removeFeeds($this.data('feedid'), function(){
+			$this.parent().fadeOut(500, function(){
+				$(this).remove();
+			});
+		});
+	});
 
 	//add event add feed
-	$("#feedURLSet #feedURLSubmit").click(function(){
+	$("#feedURLSet #feedURLSubmit").live("click", function(){
 		var url = $("#feedURLInput").val();
 		if(url.match(/(http|ftp):\/\/[!#-9A-~]+\.+[a-z0-9]+/i)){
 			method.addFeeds(0, url);
+			method.loadFeed(url, 10);
 		} else {
 			alert("URL Error");
 		}
 	});
-
-	this.loaderChecker();
-}
-
-App.minReader.prototype.loaderChecker = function(){
-	var method = this;
-	var articles = method.storage.articles();
-	var feeds    = method.storage.feeds();
-
-	if(feeds.length === articles.length){
-		method.viewFeedList();
-		return;
-	}
-
-	setTimeout(function(){
-		method.loaderChecker();
-	},100);
-}
-
-//load Feeds
-App.minReader.prototype.loader = function(){
-	var method = this;
-	var feeds = this.storage.feeds();
-	
-	for(var i = 0; i < feeds.length; i++){
-		//new feed
-		var feed = new google.feeds.Feed(feeds[i]);
-		var length = 10;
-
-		//feed length
-		feed.setNumEntries(length);
-		//feed load
-		feed.load(function(result) {
-			if (!result.error) {
-				var data = {};
-				data.title       = result.feed.title;
-				data.description = result.feed.description;
-				data.feedUrl     = result.feed.feedUrl;
-				data.url         = result.feed.link;
-				data.items       = result.feed.entries;
-				method.addArticles(0, data);
-			} else {
-				alert("Error");
-			}
-		});
-	}
-
-	
-
-}
-
-//feed Counter
-App.minReader.prototype.feedCounter = function(){
-	var feeds = this.storage.feeds();
-	return feeds.length;
-}
-
-//view
-App.minReader.prototype.viewFeedList = function(){
-	var articles = this.storage.articles();
-	var feeds = this.storage.feeds();
-	var $feedList = $("#feedList");
-
-	$feedList.find("*").remove();
-
-	for(var i in articles){
-		var feed = articles[i];
-		$feedList.append(
-			$("<li>")
-				.addClass("item")
-				.append(
-					$("<a>")
-						.addClass("delete")
-						.attr("href", "javascript:void(0)")
-						.data("feedid", i)
-						.html("×")
-				)
-				.append(
-					$("<a>")
-						.addClass("selector")
-						.attr("href", "javascript:void(0)")
-						.data("feedlink", feed['feedUrl'])
-						.html(feed['title'])
-				)
-					
-		);
-	}
 }
 
 //model
@@ -163,6 +82,153 @@ App.minReader.prototype.model = function(names){
 		}
 	}
 }
+
+//initialize
+App.minReader.prototype.init = function(){
+	var method = this;
+	var feeds = $.parseJSON(localStorage[this.names.storage.feeds]);
+	var feedCount = 0;
+
+	//set feeds
+	if(feeds){
+		this.storage.feeds(feeds);
+	} else {
+		this.storage.feeds(Array());
+	}
+
+
+	this.storage.articles(Array());
+	this.loader();
+	this.loaderChecker();
+	this.controler();
+}
+
+App.minReader.prototype.loaderChecker = function(){
+	var method = this;
+	var articles = method.storage.articles();
+	var feeds    = method.storage.feeds();
+
+	if(feeds.length === articles.length){
+		method.viewFeedList();
+		return;
+	}
+
+	setTimeout(function(){
+		method.loaderChecker();
+	},100);
+}
+
+//load Feeds
+App.minReader.prototype.loader = function(){
+	var method = this;
+	var feeds = this.storage.feeds();
+	
+	for(var i = 0; i < feeds.length; i++){
+		//add Articles
+		this.loadFeed(feeds[i], 10);
+	}
+}
+
+//load Feed
+App.minReader.prototype.loadFeed = function(url, length){
+	var method = this;
+	var feed = new google.feeds.Feed(url);
+
+	//feed length
+	feed.setNumEntries(length);
+
+	//feed load
+	feed.load(function(result) {
+		if (!result.error) {
+			var data = {};
+			data.title       = result.feed.title;
+			data.description = result.feed.description;
+			data.feedUrl     = result.feed.feedUrl;
+			data.url         = result.feed.link;
+			data.items       = result.feed.entries;
+			method.addArticles(0, data);
+			method.addFeedList(data.feedUrl, data.title, method.feedCounter() + 1);
+		} else {
+			alert("load Error");
+		}
+	});
+}
+
+//feed Counter
+App.minReader.prototype.feedCounter = function(){
+	var feeds = this.storage.feeds();
+	return feeds.length;
+}
+
+
+//Add List
+App.minReader.prototype.addFeedList = function(url, title, num){
+	this.$.feedList.prepend(
+		$("<li>")
+			.addClass("item")
+			.append(
+				$("<a>")
+					.addClass("delete")
+					.attr("href", "javascript:void(0)")
+					.data("feedid", num)
+					.html("×")
+			)
+			.append(
+				$("<a>")
+					.addClass("selector")
+					.attr("href", "javascript:void(0)")
+					.data("feedlink", url)
+					.html(title)
+			)
+				
+	);
+}
+
+//view List
+App.minReader.prototype.viewFeedList = function(){
+	var method   = this;
+	var articles = this.storage.articles();
+	var feeds    = this.storage.feeds();
+
+	this.$.feedList.find("*").remove();
+
+	for(var i in articles){
+		var feed = articles[i];
+		this.addFeedList(feed['feedUrl'], feed['title'], i);
+	}
+
+	//add new feed input
+	this.$.feedList.append(
+		$("<li>")
+			.attr("id", "feedURLSet")
+			.append(
+				$("<p>")
+					.addClass("icon")
+					.html("＋")
+			)
+			.append(
+				$("<input>")
+					.addClass("input")
+					.attr({
+						"id":"feedURLInput",
+						"type":"url"
+					})
+					.val("")
+			)
+			.append(
+				$("<a>")
+					.addClass("button")
+					.attr({
+						"href": "javascript:void(0)",
+						"id": "feedURLSubmit"
+					})
+					.html("Add Feed")
+			)
+				
+	);
+}
+
+
 
 //add Feeds
 App.minReader.prototype.addFeeds = function(key, data){
@@ -254,26 +320,6 @@ function initialize(feedURL) {
 						.attr("id","feedItem" + i)
 						.addClass("article")
 						.append(
-							$("<ul>")
-								.addClass("command")
-								.append(
-									$("<li>")
-										.addClass("item")
-										.append(
-											$("<a>")
-												.attr("href","javascript:void(0)")
-												.data("readmore",entry.link)
-												.data("feedid",i)
-												.html("続きを読む")
-												.click(function(){
-													var $this = $(this);
-													var $body = $("#feedItem" + $this.data("feedid"));
-													$body.load($this.data("readmore"));
-												})
-										)
-								)
-						)
-						.append(
 							$("<h3>")
 							.html(entry.title)
 							.addClass("title")        
@@ -308,20 +354,6 @@ google.setOnLoadCallback(function(){
 	var minReader = new App.minReader();
 	minReader.init();
 
-	$("#feedList li .selector").live('click',function(){
-		$this = $(this);
-
-		//drow
-		initialize($this.data('feedlink'));
-	});
-
-	$("#feedList li .delete").live('click',function(){
-		$this = $(this);
-
-		//drow
-		minReader.removeFeeds($this.data('feedid'),function(){
-			minReader.viewFeedList();
-		});
-	});
+	
 	
 });
